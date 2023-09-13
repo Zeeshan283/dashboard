d1697631d33f3b48ae3eb5c030cfbd44ae897b72
Zeeshan Ahmad <zeeshan.hacker,283@gmail.com>
2023-09-13 22:46:44 +0500

    category

--- resources/views/category/editcat.blade.php
+++ resources/views/category/editcat.blade.php
@@ -6,7 +6,7 @@
 
 @section('main-content')
 
-<h2>Edit Menu</h2>
+<h2>Edit Category</h2>
 <form action="{{ route('category.update', ['id' => $edit->id]) }}" method="POST" enctype="multipart/form-data">
     @csrf
     @method('PUT')
@@ -17,7 +17,7 @@
             <div class="form-group col-md-6">
                 <label for="inputtext11" class="ul-form__label">Choose Menu:</label>
                 <select class="form-control" id="customer_id" name="menu_id" data-live-search="true">
-                    <option value="" selected disabled>Select Menu</option>
+                    <option value="" selected disabled>{{ $spmenu->name}}</option>
                     @foreach ($menus as $menu)
                         <option value="{{ $menu->id}}"->{{ $menu->name}}</option>
                     @endforeach
@@ -29,7 +29,7 @@
 
             <div class="form-group col-md-6">
                 <label for="inputtext11" class="ul-form__label">Name:</label>
-                <input type="text" class="form-control" id="name"  name="name"  placeholder="Enter category name" value="{{ old ('name')}}">
+                <input type="text" class="form-control" id="name"  name="name"  placeholder="Enter category name" value="{{ $edit->name}}">
                 <span style="color: red">@error('name'){{ $message }}@enderror</span>
                 <small id="passwordHelpBlock" class="ul-form__text form-text ">
                     Name
@@ -37,7 +37,7 @@
             </div>
             <div class="form-group col-md-6">
                 <label for="inputtext11" class="ul-form__label">Commision:</label>
-                <input type="number" class="form-control" id="commission" name="commission"  placeholder="Commission" value="{{ old ('number')}}">
+                <input type="number" class="form-control" id="commission" name="commission"  placeholder="Commission" value="{{ $edit->commission}}">
                 <span style="color: red">@error('commision'){{ $message }}@enderror</span>
                 <small id="passwordHelpBlock" class="ul-form__text form-text ">
                     Commission
@@ -49,23 +49,67 @@
             <div class="form-group col-md-6">
                 <label for="inputEmail16" class="ul-form__label">Category Image:</label>
                 <div class="input-right-icon">
-                    <input type="file" class="form-control" id="inputEmail16"  name="img" >
-                    </span>
+                    <input type="file" class="form-control" id="imageInput" name="img">
+                    <img id="imagePreviewDesktop" src="{{ $edit->img }}" alt="Image Description" style="max-width: 100px; max-height: 100px;">
                 </div>
             </div>
             <div class="form-group col-md-6">
                 <label for="inputEmail16" class="ul-form__label">Category App Image:</label>
                 <div class="input-right-icon">
-                    <input type="file" class="form-control" id="inputEmail16"  name="imageforapp" >
-                    </span>
+                    <input type="file" class="form-control" id="inputEmail16" name="imageforapp">
+                    <img id="imagePreviewMobile" src="{{ $edit->imgforapp }}" alt="Image Description" style="max-width: 100px; max-height: 100px;">
                 </div>
             </div>
     </div>
-    <button type="submit">Update</button>
+    <button type="submit" class="btn btn-primary">Update</button>
 </form>
 @endsection
 
 @section('page-js')
 <script src="{{ asset('assets/js/vendor/datatables.min.js') }}"></script>
 <script src="{{ asset('assets/js/datatables.script.js') }}"></script>
+<script>
+    // Get references to the input and image elements for the desktop image
+    const imageInputDesktop = document.getElementById('imageInput');
+    const imagePreviewDesktop = document.getElementById('imagePreviewDesktop');
+
+    // Add an event listener to the desktop file input
+    imageInputDesktop.addEventListener('change', function () {
+        // Check if a file is selected
+        if (imageInputDesktop.files && imageInputDesktop.files[0]) {
+            const reader = new FileReader();
+
+            // When the file is loaded, set the desktop image source to the loaded data
+            reader.onload = function (e) {
+                imagePreviewDesktop.src = e.target.result;
+            };
+
+            // Read the selected file as a data URL (base64)
+            reader.readAsDataURL(imageInputDesktop.files[0]);
+        }
+    });
+
+    // Get references to the input and image elements for the mobile image
+    const imageInputMobile = document.getElementById('inputEmail16');
+    const imagePreviewMobile = document.getElementById('imagePreviewMobile');
+
+    // Add an event listener to the mobile file input
+    imageInputMobile.addEventListener('change', function () {
+        // Check if a file is selected
+        if (imageInputMobile.files && imageInputMobile.files[0]) {
+            const reader = new FileReader();
+
+            // When the file is loaded, set the mobile image source to the loaded data
+            reader.onload = function (e) {
+                imagePreviewMobile.src = e.target.result;
+            };
+
+            // Read the selected file as a data URL (base64)
+            reader.readAsDataURL(imageInputMobile.files[0]);
+        }
+    });
+</script>
+
+
 @endsection
+
