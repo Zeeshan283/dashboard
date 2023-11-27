68386fe00661b6ddd8833b534edff081460e74a5
Nabeel788 <123452586+Nabeel788@users.noreply.github.com>
2023-11-27 14:48:30 +0500

    a

--- app/Models/AdvertisementOrder.php
+++ app/Models/AdvertisementOrder.php
@@ -9,7 +9,7 @@ class AdvertisementOrder extends Model
 {
     use HasFactory;
     protected $table = "advertisement_order";
-    protected $fillable = array('id', 'customer_id', 'product_id', 'bill', 'name', 'display_status', 'display_time_start', 'phone', 'status', 'image', 'quantity', 'created_at', 'updated_at');
+    protected $fillable = array('id', 'customer_id', 'product_id', 'bill', 'name', 'display_status', 'display_time_start', 'display_end_start', 'phone', 'status', 'image', 'quantity', 'created_at', 'updated_at');
 
     public function user()
     {
