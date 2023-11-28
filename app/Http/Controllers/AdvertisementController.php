0b4a100978309ed96d2e1818ae057a95bed4354f
Nabeel788 <123452586+Nabeel788@users.noreply.github.com>
2023-11-28 13:18:27 +0500

    add

--- app/Http/Controllers/AdvertisementController.php
+++ app/Http/Controllers/AdvertisementController.php
@@ -13,6 +13,10 @@ class AdvertisementController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
         $data = Advertisement::all();
