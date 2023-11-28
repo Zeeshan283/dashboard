804ff81493c8598a0b94456ed3ccca1469e1d745
Nabeel788 <123452586+Nabeel788@users.noreply.github.com>
2023-11-28 13:18:45 +0500

    ad

--- app/Http/Controllers/AdvertisementSellerController.php
+++ app/Http/Controllers/AdvertisementSellerController.php
@@ -12,6 +12,10 @@ class AdvertisementSellerController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
     public function index()
     {
 
