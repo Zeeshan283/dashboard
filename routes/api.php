53e9b897a7d18ed3cf3c680cd5887fbcb1f2613b
Nabeel788 <123452586+Nabeel788@users.noreply.github.com>
2023-11-29 14:46:13 +0500

    api.php

--- routes/api.php
+++ routes/api.php
@@ -30,6 +30,8 @@ Route::middleware(['auth:api'])->group(function () {
 // Route::post('/send-verification-email', 'Auth\VerificationController@sendVerificationEmail')->middleware('auth:api');
 Route::post('/send-verification-email/{email}', [VerificationController::class, 'sendVerificationEmail']);
 
+
+// User Api 
 Route::controller(UserAPIController::class)->group(function () {
     Route::post('/register', 'register');
     Route::post('/login', 'login');
@@ -41,36 +43,30 @@ Route::controller(UserAPIController::class)->group(function () {
 
 // Auth::routes(['verify' => true]);
 
-
+// Old Api
 Route::get('/menus', [ApiController::class, 'menus']);
 Route::get('/categories/{menu_id}', [ApiController::class, 'categories']);
-Route::get('/sub_categories/{category_id}', [ApiController::class, 'subCategories']);
-
-//product Api
+Route::get('/sub_categories/{category_id}', [ApiController::class, 'subCategories123']);
 Route::get('/products', [ApiController::class, 'Products']);
 Route::get('/products/{sub_cat_Id}', [ApiController::class, 'GetSubCategoryProduct'])->whereNumber('sub_cat_Id');
 Route::get('/single-product/{id}', [ApiController::class, 'GetSingleProduct'])->whereNumber('id');
-
 Route::match(['get', 'post'], 'send-product-contact-message', [ApiController::class, 'ProductContactSendMessage']);
-Route::get('/search/product/{character}', [ApiController::class, 'SearchProduct']);
-
-Route::get('/brands', [ApiController::class, 'Brands']);
 Route::get('/home-banners', [ApiController::class, 'Home_Banners']);
 Route::get('/home-setting', [ApiController::class, 'Home_setting']);
 Route::get('/site-profile', [ApiController::class, 'Site_Profile']);
-
 Route::match(['get', 'post'], '/order-submit', [ApiController::class, 'OrderSubmit']);
-
 Route::get('/homeapi', [ApiController::class, 'homepage']);
-
 Route::post('/checkout', [ApiController::class, 'storeOrder']);
-
-
 Route::get('/user/{userId}/orders', [ApiController::class, 'getUserOrders']);
-
 Route::get('/vprofile/{id}', [ApiController::class, 'vendorprofile']);
-
 Route::get('/vcoupon/{id}', [ApiController::class, 'vendorcoupon']);
-
 Route::get('/faqs', [ApiController::class, 'FAQs']);
 Route::get('/pages', [ApiController::class, 'Pages']);
+
+// new api
+Route::get('/brands', [ApiController::class, 'Brands']); // all brand's api 
+Route::get('$2a$12$BcZ4pTuol5qjv910GkNmeQVt1QVhlsuZZzAtv59pse1cRg28ewsK/{identifier}', [ApiController::class, 'allProductAMenu']); //product by menu
+Route::get('$2a$12$8fVZGOF27MtlmuQ4VRZejH3UW1ArfKXtyH0VQHUIsncRqSaW/{identifier}', [ApiController::class, 'allProducts']); // product by category
+Route::get('MtlmuQ4Xty$2a$12$8fVZGOF27H0VQHUIsncRqSaWVRZejH3UW1ArfK/{identifier}', [ApiController::class, 'allProductSubcategories']); // product by sub-category
+Route::get('/search/product/{character}', [ApiController::class, 'SearchProduct']); // Product Search Api
+// Route::get('/subcategories/{id}', [ApiController::class, 'subcategories']);
