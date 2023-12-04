b46f43a47b69c5d7d20027406bf692fa6bc895cd
Nabeel788 <123452586+Nabeel788@users.noreply.github.com>
2023-12-04 12:38:48 +0500

    route api

--- routes/api.php
+++ routes/api.php
@@ -66,11 +66,12 @@ Route::get('/vcoupon/{id}', [ApiController::class, 'vendorcoupon']);
 Route::get('/faqs', [ApiController::class, 'FAQs']); // all faqs
 Route::get('/pages', [ApiController::class, 'Pages']); // all pages
 Route::get('/brands', [ApiController::class, 'Brands']); // all brand's api 
-Route::get('$2a$12$BcZ4pTuol5qjv910GkNmeQVt1QVhlsuZZzAtv59pse1cRg28ewsK/{identifier}', [ApiController::class, 'allProductAMenu']); //product by menu
-Route::get('$2a$12$8fVZGOF27MtlmuQ4VRZejH3UW1ArfKXtyH0VQHUIsncRqSaW/{identifier}', [ApiController::class, 'allProducts']); // product by category
-Route::get('MtlmuQ4Xty$2a$12$8fVZGOF27H0VQHUIsncRqSaWVRZejH3UW1ArfK/{identifier}', [ApiController::class, 'allProductSubcategories']); // product by sub-category
+Route::get('/p_b_menu/{identifier}', [ApiController::class, 'allProductAMenu']); //product by menu
+Route::get('/p_b_cat/{identifier}', [ApiController::class, 'allProducts']); // product by category
+Route::get('/p_b_sub/{identifier}', [ApiController::class, 'allProductSubcategories']); // product by sub-category
 Route::get('/search/product/{character}', [ApiController::class, 'SearchProduct']); // Product Search Api
-Route::get('/MtlmuQ4XtlmuQ4XtyyMtlmuQ4XtyMtyMtlmuQ4XtyMtyMtlmuQ4Xt', [ApiController::class, 'FeaturesProduct']); // 15 Features Product's
-Route::get('/Q4XtyMtyMtlMtyyMmuQ4XttlmtlmuQ4XtlmuQ4XuQ4XtyMtyMtlmu', [ApiController::class, 'SponserdProduct']); // 15 Sponserd Product's
-Route::get('/uQCtlmuQ4MtlmuQ4XtlmXtyyMtlmuQCC4XtyMtyMtllm4XtyMtyMt', [ApiController::class, 'HotProduct']); // 30 Hot Product's
-Route::get('/uQCtlmuQ4MtlmuQ4XtlmXtyyMtlmuQCC4XtyMtyMtllm4XtyMtyMt', [ApiController::class, 'HotProduct']); // Deals Of The Day Product's
+Route::get('/f_product', [ApiController::class, 'FeaturesProduct']); // 15 Features Product's
+Route::get('/s_product', [ApiController::class, 'SponserdProduct']); // 15 Sponserd Product's with Active coupons 
+Route::get('/h_product', [ApiController::class, 'HotProduct']); // 30 Hot Product's
+Route::get('/d_product', [ApiController::class, 'DealProduct']); // 10 Deals Of The Day Product's with Active coupons 
+Route::get('/home_page_all', [ApiController::class, 'homePageAll']);
