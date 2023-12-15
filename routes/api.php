ddff0454217bc2fb514c6b9d43a23e2c0806536d
Nabeel788 <123452586+Nabeel788@users.noreply.github.com>
2023-12-15 15:09:52 +0500

    api

--- routes/api.php
+++ routes/api.php
@@ -51,7 +51,6 @@ Route::get('/categories/{menu_id}', [ApiController::class, 'categories']);
 Route::get('/sub_categories/{category_id}', [ApiController::class, 'subCategories123']);
 Route::get('/products', [ApiController::class, 'Products']);
 Route::get('/products/{sub_cat_Id}', [ApiController::class, 'GetSubCategoryProduct'])->whereNumber('sub_cat_Id');
-Route::get('/single-product/{id}', [ApiController::class, 'GetSingleProduct'])->whereNumber('id');
 Route::match(['get', 'post'], 'send-product-contact-message', [ApiController::class, 'ProductContactSendMessage']);
 Route::get('/home-banners', [ApiController::class, 'Home_Banners']);
 Route::get('/home-setting', [ApiController::class, 'Home_setting']);
@@ -69,12 +68,13 @@ Route::get('/vcoupon/{id}', [ApiController::class, 'vendorcoupon']);
 Route::get('/faqs', [ApiController::class, 'FAQs']); // all faqs
 Route::get('/pages', [ApiController::class, 'Pages']); // all pages
 Route::get('/brands', [ApiController::class, 'Brands']); // all brand's api
+Route::get('/single-product/{id}', [ApiController::class, 'GetSingleProduct'])->whereNumber('id'); // Get Single Product by id 
 Route::get('/p_b_menu/{identifier}', [ApiController::class, 'allProductAMenu']); //product by menu
 Route::get('/p_b_cat/{identifier}', [ApiController::class, 'allProducts']); // product by category
 Route::get('/p_b_sub/{identifier}', [ApiController::class, 'allProductSubcategories']); // product by sub-category
 Route::get('/search/product/{character}', [ApiController::class, 'SearchProduct']); // Product Search Api
 Route::get('/f_product', [ApiController::class, 'FeaturesProduct']); // 15 Features Product's
-Route::get('/s_product', [ApiController::class, 'SponserdProduct']); // 15 Sponserd Product's with Active coupons 
+Route::get('/s_product', [ApiController::class, 'SponserdProduct']); // 15 Sponserd Product's with Active coupons
 Route::get('/h_product', [ApiController::class, 'HotProduct']); // 30 Hot Product's
-Route::get('/d_product', [ApiController::class, 'DealProduct']); // 10 Deals Of The Day Product's with Active coupons 
+Route::get('/d_product', [ApiController::class, 'DealProduct']); // 10 Deals Of The Day Product's with Active coupons
 Route::get('/home_page_all', [ApiController::class, 'homePageAll']);
