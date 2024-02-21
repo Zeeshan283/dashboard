ade8bd4e455c0891a7cb33b02bf523ea32d5fb23
Industrymall <nabeelali007786@gmail.com>
2024-02-21 16:46:48 +0500

    chat

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -205,7 +205,17 @@
         });
     </script>
 
-
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/65d5c7fc8d261e1b5f6309f4/1hn5had90';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    </script>
 </body>
 
 </html>
