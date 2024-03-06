4976c702836dcb9a84fd818126075049d48bc9d8
Industrymall <nabeelali007786@gmail.com>
2024-03-06 16:42:51 +0500

    timer

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -47,6 +47,22 @@
             vertical-align: baseline;
             border-radius: 0.25rem;
             font-weight: 600;
+        } 
+        .badge-for-timer {
+            color: #fff;
+            background-color: #2eb38d;
+            padding-right: 0.6em;
+            padding-left: 0.6em;
+            border-radius: 10rem display: inline-block;
+            padding: 0.25em 0.4em;
+            font-size: 75%;
+            font-weight: 700;
+            line-height: 1;
+            text-align: center;
+            white-space: nowrap;
+            vertical-align: baseline;
+            border-radius: 0.25rem;
+            font-weight: 600;
         }
 
         .badge-for-light {
